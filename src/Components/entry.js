3834ea51476585bcf49c14fa0a39622886ad1a74
Epik95mkw <epik955@gmail.com>
2023-04-06 18:41:13 -0400

    Add component files and packages

---
+++ src/Components/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../style.css';
+
+export default function Entry() {
+  return <div></div>;
+}
---
+++ src/Components/popup.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../style.css';
+
+export default function Popup() {
+  return <div></div>;
+}
