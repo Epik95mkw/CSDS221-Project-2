19c21621b0539101113d2adc86ca3c758823a509
Epik95mkw <epik955@gmail.com>
2023-04-07 16:24:59 -0400

    Page header

--- src/Components/table.js
+++ src/Components/table.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import '../style.css';
+import { Typography, Card, CardContent, Grid, Button } from '@mui/material';
+import { MenuIcon } from '@mui/icons-material';
+
+const header = (
+  <CardContent sx={{ color: 'white', backgroundColor: 'primary.dark' }}>
+    <Grid container className="centered">
+      <Grid item xs></Grid>
+      <Grid item xs={6} className="centered">
+        <MenuIcon />
+        <Typography>FRAMEWORKS</Typography>
+      </Grid>
+      <Grid item xs className="align-right">
+        <Button variant="contained">ADD</Button>
+      </Grid>
+    </Grid>
+  </CardContent>
+);
 
 export default function Table() {
-  return <div></div>;
+  return (
+    <Card>
+      {header}
+      <CardContent>
+        <Typography>content</Typography>
+      </CardContent>
+    </Card>
+  );
 }
