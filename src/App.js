190d5d8da6201758daa6ab7cfc04246724c194c7
Epik95mkw <epik955@gmail.com>
2023-04-11 18:28:58 -0400

    Implement MUI table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './style.css';
+import { Card } from '@mui/material';
 import Header from './Components/header.js';
 
 export default function App() {
   return (
-    <div>
+    <Card>
       <Header />
-    </div>
+
+      
+    </Card>
   );
 }
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import '../style.css';
-import { Typography, Card, CardContent, Grid, Button } from '@mui/material';
+import {
+  Typography,
+  CardContent,
+  Grid,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const header = (
-  <CardContent sx={{ color: 'white', backgroundColor: 'primary.dark' }}>
-    <Grid container className="centered">
-      <Grid item xs></Grid>
-      <Grid item xs={6} className="centered">
-        <MenuIcon />
-        <Typography>&nbsp;FRAMEWORKS</Typography>
-      </Grid>
-      <Grid item xs className="align-right">
-        <Button variant="contained">
-          <AddCircleIcon />
-          &nbsp;ADD
-        </Button>
-      </Grid>
-    </Grid>
-  </CardContent>
-);
+const columns = [
+  'Title',
+  'Description',
+  'Deadline',
+  'Priority',
+  'IsComplete',
+  'Action',
+];
+
+const columnHeaders = columns.map((c) => (
+  <TableCell align="center">{c}</TableCell>
+));
 
 export default function Header() {
   return (
-    <Card>
-      {header}
-      <CardContent>
-        <Typography>content</Typography>
+    <div>
+      <CardContent sx={{ color: 'white', backgroundColor: 'primary.dark' }}>
+        <Grid container>
+          <Grid item xs></Grid>
+          <Grid item xs={6} className="centered">
+            <MenuIcon />
+            <Typography>&nbsp;FRAMEWORKS</Typography>
+          </Grid>
+          <Grid item xs className="align-right">
+            <Button variant="contained">
+              <AddCircleIcon />
+              &nbsp;ADD
+            </Button>
+          </Grid>
+        </Grid>
       </CardContent>
-    </Card>
+
+      <CardContent>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>{columnHeaders}</TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+      </CardContent>
+    </div>
   );
 }
